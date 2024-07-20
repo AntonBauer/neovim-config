8b74d25c16748f7d2cf6a394e9bb34594b577d04
Anton Bauer <bauer_av+git@proton.me>
2024-07-20 13:53:12 +0200

    add debug keybinding to csharp plugin

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -8,5 +8,17 @@ return {
 	config = function()
 		require("mason").setup() -- Mason setup must run before csharp
 		require("csharp").setup()
+
+		vim.keymap.set("n", "<F5>", function()
+			require("csharp").debug_project()
+		end, { desc = "Debug project" })
+
+		vim.keymap.set("n", "<leader>fu", function()
+			require("csharp").fix_usings()
+		end, { desc = "[F]ix [U]sings" })
+
+		vim.keymap.set("n", "<leader>fa", function()
+			require("csharp").fix_all()
+		end, { desc = "[F]ix [A]ll" })
 	end,
 }
