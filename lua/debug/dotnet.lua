50f68d75703f2266bd20a1f3f92b4594cc43d052
Anton Bauer <bauer_av+git@proton.me>
2024-07-25 21:23:34 +0200

    add dotnet debug configuration skeleton

---
+++ lua/debug/dotnet.lua
@@ -0,0 +1,120 @@
+local M = {}
+
+local function _getOmnisharpClient(buffer)
+	local clients = vim.lsp.get_clients({ bufnr = buffer })
+	for _, client in ipairs(clients) do
+		if client.name == "omnisharp" then
+			return client
+		end
+	end
+end
+
+local function _getWorkspaceInformation()
+	local buffer = vim.api.nvim_get_current_buf()
+	local omnisharpClient = _getOmnisharpClient(buffer)
+	if omnisharpClient == nil then
+		vim.print("Omnisharp isn't attached to buffer")
+		return
+	end
+
+	local request_method = "c#/projects"
+	local request = {
+		ExcludeSourceFiles = true,
+	}
+	local response = omnisharpClient.request_sync(request_method, request, 1000, buffer)
+	if response == nil then
+		vim.print("Error: response is null")
+		return
+	end
+
+	if response.err ~= nil then
+		vim.print("Error: " .. response.err)
+	end
+
+	return response.result
+end
+
+local function _selectProject()
+	local workspaceInfo = _getWorkspaceInformation()
+	if workspaceInfo == nil then
+		vim.print("Error: fetch workspace information failed")
+		return
+	end
+
+	local executableProjects = {}
+
+	for _, project in ipairs(workspaceInfo.MsBuild.Projects) do
+		if project.IsExe then
+			table.insert(executableProjects, project)
+		end
+	end
+
+	if #executableProjects == 0 then
+		vim.print("Error: no executable project found")
+		return
+	elseif #executableProjects == 1 then
+		local project = executableProjects[1]
+	else
+		local project = executableProjects[1]
+	end
+end
+
+local function _buildProject()
+	local default_path = vim.fn.getcwd() .. "/"
+	if vim.g["dotnet_last_proj_path"] ~= nil then
+		default_path = vim.g["dotnet_last_proj_path"]
+	end
+
+	local path = vim.fn.input("Path to your *proj file", default_path, "file")
+	vim.g["dotnet_last_proj_path"] = path
+	local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+	print("")
+	print("Cmd to execute: " .. cmd)
+	local f = os.execute(cmd)
+	if f == 0 then
+		print("\nBuild: ✔️ ")
+	else
+		print("\nBuild: ❌ (code: " .. f .. ")")
+	end
+end
+
+local function _getDllPath()
+	local request = function()
+		return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+	end
+
+	if vim.g["dotnet_last_dll_path"] == nil then
+		vim.g["dotnet_last_dll_path"] = request()
+	else
+		if
+			vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2)
+			== 1
+		then
+			vim.g["dotnet_last_dll_path"] = request()
+		end
+	end
+
+	return vim.g["dotnet_last_dll_path"]
+end
+
+function M.setupDebug(dap)
+	vim.g.dotnet_build_project = _buildProject
+	vim.g.dotnet_get_dll_path = _getDllPath
+
+	local config = {
+		{
+			type = "coreclr",
+			name = "launch - netcoredbg",
+			request = "launch",
+			program = function()
+				vim.g.dotnet_build_project()
+				return vim.g.dotnet_get_dll_path()
+			end,
+		},
+	}
+
+	dap.configurations.cs = config
+	dap.configurations.fsharp = config
+end
+
+return M
--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,6 +1,46 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
+local function setupKeymaps(dap)
+	-- Basic debugging keymaps, feel free to change to your liking!
+	vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+	vim.keymap.set("n", "<leader><F5>", dap.terminate, { desc = "Debug: Terminate" })
+	vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+	vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+	vim.keymap.set("n", "<F9>", dap.step_out, { desc = "Debug: Step Out" })
+	vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+	vim.keymap.set("n", "<leader>B", function()
+		dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+	end, { desc = "Debug: Set Breakpoint" })
+end
+
+local function setupDebugUi(dapui)
+	-- Dap UI setup
+	-- For more information, see |:help nvim-dap-ui|
+	dapui.setup({
+		-- Set icons to characters that are more likely to work in every terminal.
+		--    Feel free to remove or use ones that you like more! :)
+		--    Don't feel like these are good choices.
+		icons = {
+			expanded = "",
+			collapsed = "",
+			current_frame = "*",
+		},
+		controls = {
+			icons = {
+				pause = "",
+				play = "",
+				step_into = "",
+				step_over = "",
+				step_out = "",
+				step_back = "",
+				run_last = "󰑟",
+				terminate = "󰓛",
+				disconnect = "󰇪",
+			},
+		},
+	})
+
+	-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+	vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+end
 
 return {
 	-- NOTE: Yes, you can install new plugins here!
@@ -18,9 +58,6 @@ return {
 		"jay-babu/mason-nvim-dap.nvim",
 	},
 	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-
 		require("mason-nvim-dap").setup({
 			-- Makes a best effort to setup the various debuggers with
 			-- reasonable debug configurations
@@ -38,48 +75,16 @@ return {
 			},
 		})
 
-		-- Basic debugging keymaps, feel free to change to your liking!
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-		vim.keymap.set("n", "<leader><F5>", dap.terminate, { desc = "Debug: Terminate" })
-		vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
-		vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
-		vim.keymap.set("n", "<F9>", dap.step_out, { desc = "Debug: Step Out" })
-		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		vim.keymap.set("n", "<leader>B", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end, { desc = "Debug: Set Breakpoint" })
+		local dap = require("dap")
+		setupKeymaps(dap)
 
-		-- Dap UI setup
-		-- For more information, see |:help nvim-dap-ui|
-		dapui.setup({
-			-- Set icons to characters that are more likely to work in every terminal.
-			--    Feel free to remove or use ones that you like more! :)
-			--    Don't feel like these are good choices.
-			icons = {
-				expanded = "",
-				collapsed = "",
-				current_frame = "*",
-			},
-			controls = {
-				icons = {
-					pause = "",
-					play = "",
-					step_into = "",
-					step_over = "",
-					step_out = "",
-					step_back = "",
-					run_last = "󰑟",
-					terminate = "󰓛",
-					disconnect = "󰇪",
-				},
-			},
-		})
-
-		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-		vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
+		local dapui = require("dapui")
+		setupDebugUi(dapui)
 
 		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+		require("debug.dotnet").setupDebug(dap)
 	end,
 }
